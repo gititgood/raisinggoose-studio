1b6d0fb243965ee73c1b5e23571440634feca2a0
gititgood <nvl789@gmail.com>
2025-08-21 14:46:24 -0700

    "added new fileds for duration, location and timeToCalm"

--- schemas/stimEvents.js
+++ schemas/stimEvents.js
@@ -8,5 +8,8 @@ export default defineType({
     defineField({ name: 'intensity', title: 'Intensity (1-5)', type: 'number', validation: r => r.min(1).max(5) }),
     defineField({ name: 'notes', title: 'Notes', type: 'string' }),
     defineField({ name: 'category', title: 'Category', type: 'string', options: { list: ['start','peak','resolve'] } }),
+    defineField({ name: 'duration', title: 'Duration (seconds)', type: 'number', validation: r => r.min(0) }),
+    defineField({ name: 'location', title: 'Location', type: 'string' }),
+    defineField({ name: 'timeToCalm', title: 'Time to Calm (seconds)', type: 'number', validation: r => r.min(0) })
   ],
 })
