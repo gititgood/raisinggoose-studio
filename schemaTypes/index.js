3ac6edca4914bb68eb2cf8b9dd2255a2aecce2d1
gititgood <nvl789@gmail.com>
2025-08-21 14:22:23 -0700

    moved schemas folder to main folder outside of runtime

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,7 +2,7 @@ import blockContent from './blockContent.js'
 import category from './category.js'
 import post from './post.js'
 import author from './author.js'
-import stimEvents from '../.sanity/runtime/schemas/stimEvents.js'
-import observations from '../.sanity/runtime/schemas/observations.js'
+import stimEvents from '../schemas/stimEvents.js'
+import observations from '../schemas/observations.js'
 
 export const schemaTypes = [post, author, category, blockContent, stimEvents, observations]
