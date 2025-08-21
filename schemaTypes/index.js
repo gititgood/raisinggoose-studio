1815c30ae40b65cdcd178f4a6b48133079e065b0
gititgood <nvl789@gmail.com>
2025-08-21 13:58:52 -0700

    fix(studio): ESM imports with .js extensions

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,8 +1,8 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import stimEvents from '../.sanity/runtime/schemas/stimEvents'
-import observations from '../.sanity/runtime/schemas/observations'
+import blockContent from './blockContent.js'
+import category from './category.js'
+import post from './post.js'
+import author from './author.js'
+import stimEvents from '../.sanity/runtime/schemas/stimEvents.js'
+import observations from '../.sanity/runtime/schemas/observations.js'
 
 export const schemaTypes = [post, author, category, blockContent, stimEvents, observations]
